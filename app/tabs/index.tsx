8ab126c5d4a96aa1cfbc28505d6ba10b37fb64e4
mihailgaberov <mihail.gaberov@gmail.com>
2024-10-26 14:41:25 +0200

    feat: Add image picker

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,13 +1,20 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+
+import ImageViewer from "@/app/components/ImageViewer";
+import Button from "@/app/components/Button";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen.</Text>
-      <Link href="/tabs/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -18,12 +25,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
