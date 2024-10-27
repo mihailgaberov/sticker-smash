94170aae2a85e4009a092baf3af5d548db77b737
mihailgaberov <mihail.gaberov@gmail.com>
2024-10-27 08:48:39 +0100

    feat: Adding gesture handling

--- app/components/EmojiSticker.tsx
+++ app/components/EmojiSticker.tsx
@@ -1,16 +1,46 @@
-import { View } from "react-native";
-import { Image } from "expo-image";
+import { View, Image, ImageSourcePropType } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
 type Props = {
   imageSize: number;
   stickerSource: string;
 };
 
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+  const scaleImage = useSharedValue(imageSize);
+
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      } else {
+        scaleImage.value = Math.round(scaleImage.value / 2);
+      }
+    });
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
+  // Convert stickerSource to ImageSourcePropType
+  const imageSource: ImageSourcePropType = { uri: stickerSource };
+
   return (
     <View style={{ top: -350 }}>
-      <Image
-        source={stickerSource}
+      <AnimatedImage
+        source={imageSource}
+        resizeMode="contain"
         style={{ width: imageSize, height: imageSize }}
       />
     </View>
